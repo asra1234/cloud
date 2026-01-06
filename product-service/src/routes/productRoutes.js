e91b0fe33d255e9964a3c2db9ad8cd9d35c2ee56
Azra suhair <suhairazra@gmail.com>
2026-01-06 19:37:18 +0530

    updated product

--- product-service/src/routes/productRoutes.js
+++ product-service/src/routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const auth = require("../middleware/auth");
+const auth = require("../middleware/authMiddleware");
 const controller = require("../controllers/productController");
 
 router.post("/", auth, controller.createProduct);
