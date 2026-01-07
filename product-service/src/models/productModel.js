b772dca36255665f217f691469c239be5a8ebddf
Azra suhair <suhairazra@gmail.com>
2026-01-07 08:23:15 +0530

    corrected

--- product-service/src/models/productModel.js
+++ product-service/src/models/productModel.js
@@ -1,10 +1,25 @@
 const db = require("./db");
 
-exports.create = (data) =>
-  db.query(
-    "INSERT INTO products (name, price) VALUES (?, ?)",
-    [data.name, data.price]
+exports.create = (data) => {
+  return db.query(
+    `INSERT INTO products 
+    (name, slug, category_id, brand_id, price, description, is_active, is_featured, in_stock, on_sale)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+    [
+      data.name,
+      data.slug,
+      data.category_id,
+      data.brand_id,
+      data.price,
+      data.description,
+      data.is_active,
+      data.is_featured,
+      data.in_stock,
+      data.on_sale
+    ]
   );
+};
+
 
 exports.getAll = () => db.query("SELECT * FROM products");
 
